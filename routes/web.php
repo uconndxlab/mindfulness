9282804a1494b50db531e9d672c77edd22781c94
WillSho1 <whshocon@gmail.com>
2024-04-16 13:45:54 -0400

    Made a notes data base from following video. Made a factory, seeder, model, controller, and routes for CRUD operations on notes. More to work on, but learning how the framework works. Moving towards makingviews for skeleton structure of app

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PageNavController;
+use App\Http\Controllers\NoteController;
 
-#template
-Route::get('/', function () {
-    return view('welcome');
+//template
+Route::get('/zzoldpage', function () {
+    return view('welcomeold');
 });
 
 
-//login
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('show.login');
 
+//login
+Route::get('/', [AuthController::class, 'showLoginForm'])->name('show.login');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('show.login');
 
 //registration
 Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('show.registration');
 
+
+//Page Navigation
+Route::get('/welcome', [PageNavController::class, 'welcome'])->name('welcome');
+
+
+//notes CRUD routes
+//TODO should not be web accessible
+Route::resource('note', NoteController::class);
+// Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+// Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+// Route::post('/note', [NoteController::class,'store'])->name('note.store');
+// Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+// Route::post('/note/{id}/edit', [NoteController::class,'edit'])->name('note.edit');
+// Route::put('/note/{id}', [NoteController::class,'update'])->name('note.update');
+// Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
