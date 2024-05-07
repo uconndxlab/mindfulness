254fb6e75c5969a89c4b8dced30e891a4e4f2c41
WillSho1 <whshocon@gmail.com>
2024-05-07 18:37:45 -0400

    adjusted the layout for auth pages - getting used to containers/centering and layout. finished welcome page

--- resources/views/auth/welcome.blade.php
+++ resources/views/auth/welcome.blade.php
@@ -3,8 +3,17 @@
 @section('title', 'Welcome!')
 
 @section('content')
-<div>
-    Welcome page - give introduction video
-    <a href="{{ route('voiceSelect') }}">NEXT</a>
+<div class="col-md-5">
+    <div class="text-left">
+        <h1 class="display-3 font-weight-bold">Welcome!</h1>
+        <p class="lead">Welcome text goes here. It can wrap all the way to the next line.</p>
+    </div>
+    <div class="text-center">
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/LtNYaH61dXY?si=RyyPvLtKQyNmZPSE&amp;controls=0" allowfullscreen></iframe>
+        </div>
+        <br><br>
+        <a href="{{ route('voiceSelect') }}" class="btn btn-primary btn-lg">Next</a>
+    </div>
 </div>
 @endsection
