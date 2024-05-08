9dcbe4d6a3765bc2fc4051b5fc51464101cf32fb
BuildTools <unconfigured@null.spigotmc.org>
2024-05-08 14:47:16 -0400

    Migration to add word of the day to notes table. Adjusted notes model

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'note',
+        'user_id',
+        'word_otd',
+    ];
 }
