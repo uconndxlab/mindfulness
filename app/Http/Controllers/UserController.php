a91da3eaebcead9be20181d94fc9bafa12fecfe3
WillSho1 <whshocon@gmail.com>
2024-05-07 20:33:49 -0400

    done with welcome and voice select page. migration to add voiceId COL to USERS table. voice select form functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function updateVoice(Request $request) {
+        //check for voiceId
+        if ($request->filled('voiceId')) {
+            //get user
+            $user = Auth::user();
+    
+            //update
+            $user->voiceId = $request->input('voiceId');
+            $user->save();
+        }
+
+        return redirect(route('explore.home'));
+    }
 }
--- resources/views/auth/voice-select.blade.php
+++ resources/views/auth/voice-select.blade.php
@@ -3,8 +3,32 @@
 @section('title', 'Welcome!')
 
 @section('content')
-<div>
-    Select the voice on this page after account creation
-    <a href="{{ route('explore.home') }}">NEXT</a>
+
+<div class="col-md-5 border">
+    <div class="text-left">
+        <h1 class="display font-weight-bold">Voice Selection</h1>
+        <p>Please select one of the following voices. This can be changed later on.</p>
+    </div>
+    <form action="{{ route('user.update.voice') }}" method="POST">
+        @method('PUT')
+        @csrf
+        <div class="form-group">
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="voiceId" id="voice1" value="someName">
+                <label class="form-check-label" for="voice1">
+                    First Voice
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="voiceId" id="voice2" value="otherName">
+                <label class="form-check-label" for="voice2">
+                    Second Voice
+                </label>
+            </div>
+        </div>
+        <div class="text-center">
+            <button type="submit" class="btn btn-primary btn-lg">NEXT</button>
+        </div>
+    </form>
 </div>
 @endsection
