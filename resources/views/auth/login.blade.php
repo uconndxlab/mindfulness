cb28015cb0da4dbf59048f6d6dc95fb8b7df44ef
BuildTools <unconfigured@null.spigotmc.org>
2024-05-09 13:09:07 -0400

    fixed old-value issue on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
 
         <div class="form-group">
             <label for="email">Email</label>
-            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror @error('password') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror @error('password') is-invalid @enderror" name="email" value="{{ old('email') }}">
             @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -19,7 +19,7 @@
 
         <div class="form-group">
             <label for="password">Password</label>
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password">
             @error('password')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
