b0607039662236be66de1d5032687117c26bea31
BuildTools <unconfigured@null.spigotmc.org>
2024-05-21 12:10:05 -0400

    Added edit and delete functionality on lessons for admins.

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\PageNavController;
+use Illuminate\Support\Facades\Storage;
 
 class ContentController extends Controller
 {
@@ -104,11 +105,87 @@ class ContentController extends Controller
         return view('admin.lessonUpload', compact('showBackBtn', 'hideBottomNav', 'hideProfileLink', 'modules', 'lesson'));
     }
 
-    public function updateLesson($lessonId) {
-        
+    public function updateLesson(Request $request, $lessonId) {
+        try {
+            $request->validate([
+                'title' => ['required', 'string', 'max:255'],
+                'module' => ['required', 'exists:modules,id'],
+                'description' => ['nullable', 'string', 'max:1027'],
+                'file'=> ['nullable', 'mimes:mp4,mp3,wav,ogg', 'max:40960'],
+            ]);
+        } catch (ValidationException $e) {
+            return redirect()->back()->withErrors($e->errors())->withInput();
+        }
+
+        $lesson = Lesson::find($lessonId);
+        $lesson->title = $request->title;
+        if ($lesson->module_id != $request->module) {
+            //lesson count
+            $module = Module::find($lesson->module_id);
+            $module->lesson_count = Lesson::where('module_id', $module->id)->count() + 1;
+            $module->save();
+
+            //adjusting lesson numbers
+            $lessonsAfter = Lesson::where('lesson_number' > $lesson->lesson_number);
+            foreach ($lessonsAfter as $lessonAfter) {
+                $lessonAfter->lesson_number--;
+                $lessonAfter->save();
+            }
+
+            //assign lesson to module
+            $lesson->module_id = $request->module;
+            $lesson->lesson_number = $module->lesson_count;
+        }
+        $lesson->description = $request->description;
+
+        //file check
+        $sameFile = false;
+        if ($request->hasFile('file')) {
+            $newFile = $request->file('file');
+            $newFileHash = hash_file('md5', $newFile->getRealPath());
+            //if path saved in db
+            if ($lesson->file_path) {
+                $currentFilePath = storage_path('app/'.$lesson->file_path);
+                //if file exists
+                if (file_exists($currentFilePath)) {
+                    $currentFileHash = hash_file('md5', $currentFilePath);
+                    //if file is not the same
+                    if ($newFileHash !== $currentFileHash) {
+                        //delete old
+                        Storage::disk()->delete($lesson->file_path);
+                    }
+                    else {
+                        $sameFile = true;
+                    }
+                }
+            }
+            if (!$sameFile) {
+                //save new
+                $newFilePath = $newFile->store('content');
+                $lesson->file_path = $newFilePath;
+            }
+        }
+
+        //DELETE FILE if not passed in?
+
+        $lesson->save();
+        return redirect()->route('admin.browse')->with('success', 'Lesson updated successfully.');
     }
 
     public function deleteLesson($lessonId) {
-        
+        $lesson = Lesson::find($lessonId);
+        //delete content
+        if ($lesson->file_path) {
+            if (file_exists(storage_path('app/'.$lesson->file_path))) {
+                Storage::disk()->delete($lesson->file_path);
+            }
+        }
+        $module = Module::find($lesson->module_id);
+        $lesson->delete();
+        //adjust module
+        $module->lesson_count = Lesson::where('module_id', $module->id)->count();
+        $module->save();
+
+        return redirect()->route('admin.browse')->with('success', 'Lesson deleted.');
     }
 }
