449623d0054266c5a9e57ca8855d23620d1939c6
BuildTools <unconfigured@null.spigotmc.org>
2024-05-21 14:41:24 -0400

    Added quiz inputs to form. Made quizzes table in database. Adjusted Lesson and Quiz models to fit relationship

---
+++ app/Models/Quiz.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quiz extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'lesson_id', 
+        'question', 
+        'options_feeback', 
+        'correct_answer'
+    ];
+
+    protected $casts = [
+        'options' => 'array',
+    ];
+
+    public function lesson()
+    {
+        return $this->belongsTo(Lesson::class);
+    }
+}
