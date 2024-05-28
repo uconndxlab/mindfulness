639e6abfad28dd563fbbaa9440ab35bfbf353eb0
WillSho1 <whshocon@gmail.com>
2024-05-28 12:21:06 -0400

    Minor changes, blocked admin pages.

--- resources/views/profile/accountInformation.blade.php
+++ resources/views/profile/accountInformation.blade.php
@@ -53,7 +53,7 @@
     @if (auth::user()->isAdmin())
         <div class="text-center">
             <div class="form-group">
-                <a href="{{ route('admin.browse') }}" class="btn btn-primary">ADMIN: Content Upload</a>
+                <a href="{{ route('admin.browse') }}" class="btn btn-primary disabled">ADMIN: Content Upload</a>
             </div>
         </div>
     @endif
