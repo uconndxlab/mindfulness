f886832d7ce33057cfc136981cdb04cc96f93d95
WillSho1 <whshocon@gmail.com>
2024-07-16 18:48:55 -0400

    bug fix - route missing from completed module

--- resources/views/explore/home.blade.php
+++ resources/views/explore/home.blade.php
@@ -15,7 +15,7 @@
                     <div class="h-100">
                         <div class="card p-2 module mb-2">
                             @if ($module->progress['status'] == 'completed')
-                                <a id="moduleLink" class="stretched-link w-100">
+                                <a id="moduleLink" class="stretched-link w-100" href="{{ route('explore.module', ['module_id' => $module->id]) }}">
                                     <i class="bi bi-check2-square"></i>
                                     {{ $module->name }} - Completed: {{$module->progress['completed']}}/{{$module->progress['total']}}
                                 </a>
