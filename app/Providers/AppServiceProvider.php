134a18e8d845ffaf4a90a5b9c2557d2aadf48601
WillSho1 <whshocon@gmail.com>
2024-09-23 13:05:34 -0400

    resolved remember me issue - set to 7 days

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Events\FinalActivityCompleted;
 use App\Listeners\ShowCompletionModal;
 use Event;
+use Illuminate\Auth\SessionGuard;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
@@ -28,10 +29,16 @@ class AppServiceProvider extends ServiceProvider
             [ShowCompletionModal::class, 'handle']
         );
 
-        //remember me for 7 days
-        Auth::extend('session', function ($app, $name, array $config) {
-            $guard = new \Illuminate\Auth\SessionGuard($name, Auth::createUserProvider($config['provider']), $app['session.store'], $app['request']);
+        Auth::extend('custom-session', function ($app, $name, array $config) {
+            $provider = Auth::createUserProvider($config['provider']);
+            $guard = new SessionGuard($name, $provider, $app['session.store'], $app['request']);
+            //set cookie jar
+            if (method_exists($guard, 'setCookieJar')) {
+                $guard->setCookieJar($app['cookie']);
+            }
+            //remember duration - 7 days
             $guard->setRememberDuration(10080);
+
             return $guard;
         });
     }
--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
+        'guard' => 'custom-session',
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
@@ -36,6 +36,10 @@ return [
     */
 
     'guards' => [
+        'custom-session' => [
+            'driver' => 'custom-session',
+            'provider' => 'users',
+        ],
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
