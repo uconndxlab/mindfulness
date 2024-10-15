70be9e534ed7f47174f160d0b047ca0d6c876751
WillSho1 <whshocon@gmail.com>
2024-10-15 13:57:32 -0400

    small change for seeding day 5

--- database/seeders/RestructureSeeder.php
+++ database/seeders/RestructureSeeder.php
@@ -40,7 +40,7 @@ class RestructureSeeder extends Seeder
 
         //populating extra example days
         foreach (Module::all() as $module) {
-            for ($j = $module->id == 1 ? 5 : 1; $j <= 5; $j++) {
+            for ($j = $module->id == 1 ? 6 : 1; $j <= 5; $j++) {
                 $day = Day::create([
                     'module_id' => $module->id,
                     'name' => 'Day '.$j,
