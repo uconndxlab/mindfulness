52bd4b54ed2aeaef546d51bc5096022c7d9934c0
WillSho1 <whshocon@gmail.com>
2024-11-04 20:09:20 -0500

    new seeder to seed custom progress for easier testing

---
+++ database/seeders/ForceProgress.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Activity;
+use App\Models\User;
+use App\Models\UserActivity;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ForceProgress extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $order = 9;
+        $user_name = 'Zoro';
+        // find user and list of activities up to point
+        $user_id = User::where('name', $user_name)->first()->id;
+        $activity_ids = Activity::where('order', '<=', $order)->pluck('order', 'id')->toArray();
+
+        // update user progress to complete
+        foreach ($activity_ids as $activity_id => $activity_order) {
+            $status = $activity_order == $order ? 'unlocked' : 'completed';
+            UserActivity::updateOrCreate([
+                'user_id' => $user_id,
+                'activity_id' => $activity_id
+            ], [
+                'status' => $status
+            ]);
+        }
+    }
+}
