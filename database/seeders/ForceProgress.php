834f6ca86382e07e95192970293f892a23a78724
WillSho1 <whshocon@gmail.com>
2025-01-02 21:32:05 -0500

    rework forceprogress seeder

--- database/seeders/ForceProgress.php
+++ database/seeders/ForceProgress.php
@@ -16,14 +16,14 @@ class ForceProgress extends Seeder
     public function run(): void
     {
         $order = 25;
-        $user_name = 'Zoro';
+        $email = '';
         // find user and list of activities up to point
-        $user_id = User::where('name', $user_name)->first()->id;
-        $activity_ids = Activity::where('order', '<=', $order)->pluck('order', 'id')->toArray();
+        $user_id = User::where('email', $email)->first()->id;
+        $activity_ids = Activity::all()->pluck('order', 'id')->toArray();
 
         // update user progress to complete
         foreach ($activity_ids as $activity_id => $activity_order) {
-            $status = $activity_order == $order ? 'unlocked' : 'completed';
+            $status = $activity_order == $order ? 'unlocked' : ($activity_order < $order ? 'completed' : 'locked');
             UserActivity::updateOrCreate([
                 'user_id' => $user_id,
                 'activity_id' => $activity_id
