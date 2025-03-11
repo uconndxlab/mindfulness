10ff92cce9db2759202ee2f7fe036c9e71f8f630
WillSho1 <whshocon@gmail.com>
2025-03-11 15:24:42 -0400

    testing column del on dev

---
+++ database/migrations/2025_03_11_183418_remove_progress_tables.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('activities', callback: function (Blueprint $table) {
+            $table->dropColumn(['deleted']);
+        });
+        // Schema::dropIfExists('user_activity');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('activities', callback: function (Blueprint $table) {
+            $table->boolean('deleted')->default(false);
+        });
+    }
+};
