1dd744e8d645fbe795aef738bb1114205b566d6c
WillSho1 <whshocon@gmail.com>
2025-07-08 15:59:08 -0400

    swap admin seeder for change role command

---
+++ app/Console/Commands/ChangeRole.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use App\Models\Role;
+
+class ChangeRole extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'hh:role {email} {role}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Change a user\'s role by providing their email and the role.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $email = $this->argument('email');
+        $role = $this->argument('role');  
+
+        $user = User::where('email', $email)->first();
+        if (!$user) {
+            $this->error("User under '{$email}' not found");
+            return 1;
+        }
+
+        if (!in_array($role, ['admin', 'user'])) {
+            $this->error("Role '{$role}' not found");
+            return 1;
+        }
+    
+        $user->role = $role;
+        $user->save();
+        $this->info("Role '{$role}' assigned to user '{$user->email}'");
+        return 0;
+    }
+}
