401da40dc8de0124f76abca8a3abe23e29607b67
WillSho1 <whshocon@gmail.com>
2025-07-30 15:23:49 -0400

    move icons

---
+++ database/seeders/ActivitySeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Activity;
+use Illuminate\Support\Facades\File;
+
+class ActivitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $json = File::get(database_path('data/activities.json'));
+        $activities = json_decode($json);
+
+        foreach ($activities as $activity) {
+            Activity::updateOrCreate(
+                ['id' => $activity->id],
+                [
+                    'day_id' => $activity->day_id,
+                    'title' => $activity->title,
+                    'type' => $activity->type,
+                    'order' => $activity->order,
+                    'optional' => $activity->optional,
+                ]
+            );
+        }
+    }
+}
