11978cfccbd0266544782866a44d282e91bba3c3
WillSho1 <whshocon@gmail.com>
2025-07-31 17:49:48 -0400

    icons + command fix

--- app/Console/Commands/BackfillUsersHhIds.php
+++ app/Console/Commands/BackfillUsersHhIds.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\User;
 
-class BackfillUserHhIds extends Command
+class BackfillUsersHhIds extends Command
 {
     /**
      * The name and signature of the console command.
