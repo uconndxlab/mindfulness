f7a11414658422d613d2ee2509404d933395113f
WillSho1 <whshocon@gmail.com>
2025-08-11 12:18:58 -0400

    update env

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // $middleware->trustProxies(at: '*');
         $middleware->alias([
             'admin' => AdminOnly::class,
             'update.last.active' => UpdateLastActiveAt::class,
