673bec9f4b3404488c173326e0bd98a888c4cde4
WillSho1 <whshocon@gmail.com>
2025-09-11 11:22:21 -0400

    small fix remove line

--- resources/views/explore/module.blade.php
+++ resources/views/explore/module.blade.php
@@ -24,9 +24,6 @@
             @php
                 $disabled = $day->unlocked ? '' : 'disabled';
             @endphp
-            @if ($day->is_check_in)
-                <hr>
-            @endif
 
             <div class="accordion-item border mb-2" id="day_{{ $day->id }}">
                 <h2 class="accordion-header" id="heading_{{ $index }}">
