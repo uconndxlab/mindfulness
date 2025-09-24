b25a5e64aeecade1652a243a5be43447728b7f29
WillSho1 <whshocon@gmail.com>
2025-09-24 12:07:45 -0400

    actual fix - viewBox

--- resources/views/components/audio-player.blade.php
+++ resources/views/components/audio-player.blade.php
@@ -1,7 +1,7 @@
 <div id="player-{{ $id }}" class="slide__audio js-audio col-12" data-allow-seek="{{ $allowSeek ? 'true' : 'false' }}" data-allow-playback-rate="{{ $allowPlaybackRate ? 'true' : 'false' }}">
     <audio id="audio-{{ $id }}" class="slide__audio-player media-player" preload="auto" src="{{ Storage::url('content/'.$file) }}"></audio>
     <div class="audio__controls">
-        <svg version="1.1" id="circle" width="300px" height="300px" viewBox="0 0 100 100">
+        <svg version="1.1" id="circle" width="300px" height="300px" viewBox="-5 -5 110 110">
             <path id="track" fill="none" stroke-meterlimit="10" d="M50,2.9L50,2.9C76,2.9,97.1,24,97.1,50v0C97.1,76,76,97.1,50,97.1h0C24,97.1,2.9,76,2.9,50v0C2.9,24,24,2.9,50,2.9z"/>
             <path id="watched-progress" fill="none" stroke-meterlimit="10" d="M50,2.9L50,2.9C76,2.9,97.1,24,97.1,50v0C97.1,76,76,97.1,50,97.1h0C24,97.1,2.9,76,2.9,50v0C2.9,24,24,2.9,50,2.9z"/>
             <path id="seekbar" fill="none" stroke-meterlimit="10" d="M50,2.9L50,2.9C76,2.9,97.1,24,97.1,50v0C97.1,76,76,97.1,50,97.1h0C24,97.1,2.9,76,2.9,50v0C2.9,24,24,2.9,50,2.9z"/>
