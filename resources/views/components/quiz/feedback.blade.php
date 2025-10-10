950b0849ae9c5212fc8de71cdbe16716b01b7dc9
WillSho1 <whshocon@gmail.com>
2025-10-09 20:09:35 -0400

    realign feedback time

--- resources/views/components/quiz/feedback.blade.php
+++ resources/views/components/quiz/feedback.blade.php
@@ -7,11 +7,11 @@
             <div class="feedback-audio js-audio col-12 mb-2">
                 <audio id="fbAudio_{{ $question['number'] }}_{{ $option['id'] }}" class="feedback-aduio-player media-player" preload="auto" src="{{ Storage::url('content/'.$option['audio_path']) }}"></audio>
                 <div class="feedback-audio__controls d-flex justify-content-between">
-                    <div class="d-flex">
+                    <div class="d-flex align-items-center">
                         <button class="play-pause" type="button">
                             <i id="icon" class="bi bi-play"></i>
                         </button>
-                        <div class="d-flex mt-2 ms-2"><small id="current-time"></small><small>/</small><small id="max-time"></small></div>
+                        <div class="ms-2"><small id="current-time"></small><small>/</small><small id="max-time"></small></div>
                     </div>
                     <div class="dropdown">
                         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
