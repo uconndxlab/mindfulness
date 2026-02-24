45d6f809979059cd2da870f0127f5c347c1ee061
WillSho1 <whshocon@gmail.com>
2026-02-23 19:04:57 -0500

    tool tip/increase slider question font size

--- resources/views/components/quiz/question.blade.php
+++ resources/views/components/quiz/question.blade.php
@@ -6,6 +6,18 @@
      
     <div class="d-flex align-items-center gap-2 quiz-question mb-3">
         @markdown($question['question'])
+        @if (isset($question['tooltip']) && $question['tooltip'] !== '')
+            <strong class="text-link fs-5"
+                role="button"
+                tabindex="0"
+                data-bs-toggle="popover"
+                data-bs-trigger="hover"
+                data-bs-placement="top"
+                data-bs-html="true"
+                data-bs-content="{{ $question['tooltip'] }}">
+                <i class="bi bi-info-circle" ></i>
+            </strong>
+        @endif
     </div>
 
     @include('components.quiz.question-types', ['question' => $question])
