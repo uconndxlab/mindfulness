036f95bcb9dda995777a13a50e654181c410d6cd
WillSho1 <whshocon@gmail.com>
2026-02-23 20:40:57 -0500

    survey question blade template

--- resources/views/components/quiz/question-types.blade.php
+++ resources/views/components/quiz/question-types.blade.php
@@ -91,4 +91,50 @@
             <span id="slider_average_value_{{ $question['number'] }}" class="pq-score">--</span>%
         </div>
     </div>
+@elseif ($question['type'] === 'survey')
+    @foreach ($question['options'] as $index => $option)
+        <hr>
+        <label class="form-label">
+            <div class="quiz-survey-label">
+                @markdown($option['text'])
+            </div>
+        </label>
+        <div class="quiz-survey mb-3 d-flex justify-content-center align-items-start flex-wrap">
+            @foreach ($option['survey_config']['options'] ?? [] as $value => $label)
+                @if (!$loop->first)
+                    <div class="vr flex-shrink-0 mx-2 align-self-stretch"></div>
+                @endif
+                <label class="survey-option-cell d-flex flex-column align-items-center text-center px-3 py-2"
+                    for="survey_{{ $question['number'] }}_{{ $option['id'] }}_{{ $value }}">
+                    <span class="survey-option-value fw-bold small text-secondary">{{ $value }}</span>
+                    <span class="survey-option-label small">{{ $label }}</span>
+                    <input class="form-check-input mt-1"
+                        type="radio"
+                        name="answer_{{ $question['number'] }}[{{ $option['id'] }}]"
+                        id="survey_{{ $question['number'] }}_{{ $option['id'] }}_{{ $value }}"
+                        value="{{ $value }}">
+                </label>
+            @endforeach
+        </div>
+        @if ($index === count($question['options']) - 1)
+            <hr>
+        @endif
+    @endforeach
+    <div class="d-flex justify-content-end">
+        <div id="survey_average_display_{{ $question['number'] }}" class="text-muted d-none">
+            <strong class="text-link"
+                role="button"
+                tabindex="0"
+                data-bs-toggle="popover"
+                data-bs-trigger="hover"
+                data-bs-placement="top"
+                data-bs-html="true"
+                data-bs-title="Practice Quality"
+                data-bs-content="This percentage reflects how consistently you returned to your present-moment experience during the practice. A higher score indicates you spent more time being aware and accepting, rather than avoiding or pushing away experiences.">
+                <i class="bi bi-info-circle" ></i> 
+                Practice Quality:
+            </strong> 
+            <span id="survey_average_value_{{ $question['number'] }}" class="pq-score">--</span>
+        </div>
+    </div>
 @endif
