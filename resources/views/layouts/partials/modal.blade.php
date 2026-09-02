1560a36cce4f158983facfcbfc13cb5195b23e7b
WillSho1 <whshocon@gmail.com>
2026-09-02 10:50:25 -0400

    center modals

--- resources/views/layouts/partials/modal.blade.php
+++ resources/views/layouts/partials/modal.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="appModal" tabindex="-1" aria-labelledby="appModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="appModalLabel"></h5>
